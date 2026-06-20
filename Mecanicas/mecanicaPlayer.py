2ca84afafd5f59f81f6112114d994b510f867ec1
juantarcisio1 <juantarcisio081@gmail.com>
2026-06-20 20:15:59 -0300

    Merge branch 'inimigo' of https://github.com/andradegabrielDEV2007/Projeto-Final-IP-Pygame into inimigo

--- Mecanicas/mecanicaPlayer.py
+++ Mecanicas/mecanicaPlayer.py
@@ -9,25 +9,32 @@ class Jogador:
         self.altura = 30
         self.velocidade = 5
         self.cor = (0, 200, 100)
+        self.direcao_da_frente = "direita"  # Inicialmente, o jogador está olhando para a direita
+        self.cooldown_tiro = 300  # Tempo em milissegundos (300 = meio segundo)
+        self.ultimo_tiro = 0      # Relógio zera quando o jogo começa
 
     def mover(self):
         teclas = pygame.key.get_pressed()
 
         if teclas[pygame.K_LEFT] or teclas[pygame.K_a]:
             if self.x > 0 :
-                self.x -= self.velocidade
+                self.x -= self.velocidade #anda pra esquerda
+                self.direcao_da_frente = "esquerda"  # Atualiza a direção da frente para esquerda
 
         if teclas[pygame.K_RIGHT] or teclas[pygame.K_d]:
             if self.x < 770 :
                 self.x += self.velocidade
+                self.direcao_da_frente = "direita" 
 
         if teclas[pygame.K_UP] or teclas[pygame.K_w]:
             if self.y > 0 :
                 self.y -= self.velocidade
+                self.direcao_da_frente = "cima"
 
         if teclas[pygame.K_DOWN] or teclas[pygame.K_s]:
             if self.y < 570 :
                 self.y += self.velocidade
+                self.direcao_da_frente = "baixo"
 
     def desenhar(self, tela):
         retangulo = pygame.Rect(self.x, self.y, self.largura, self.altura)
--- sistemas/jogo.py
+++ sistemas/jogo.py
@@ -1,6 +1,9 @@
 import pygame
 import sys
 from Mecanicas.mecanicaPlayer import Jogador 
+from entidades.projetil import Projetil
+
+
 
 class Jogo:
     def __init__(self):
@@ -12,6 +15,7 @@ class Jogo:
 
         # Cria o jogador usando a classe que veio da Mecanicas
         self.player = Jogador(400, 300)
+        self.grupo_projeteis = pygame.sprite.Group() #grupo para armazenar os projeteis serve para atualizar e desenhar todos os projeteis de uma vez
 
     def rodar(self):
         # loop 
@@ -29,24 +33,37 @@ class Jogo:
             if evento.type == pygame.QUIT:
                 self.rodando = False
 
-    def atualizar(self):
+            if evento.type == pygame.KEYDOWN:
+                if evento.key == pygame.K_ESCAPE:
+                    self.rodando = False
+
+                if evento.key == pygame.K_SPACE:  
+                    tempo_atual = pygame.time.get_ticks()
+                    if tempo_atual - self.player.ultimo_tiro >= self.player.cooldown_tiro:
+                        px = self.player.x
+                        py = self.player.y
+                        direcao = self.player.direcao_da_frente
+                        novo_tiro = Projetil(px, py, direcao)
+                        self.grupo_projeteis.add(novo_tiro)
+                        self.player.ultimo_tiro = tempo_atual
+
+    def update(self):
         # calculando o movimento do jogador
         self.player.mover()
+        # atualizando os projeteis
+        self.grupo_projeteis.update()
 
     def desenhar(self):
-        self.tela.fill((40, 44, 52)) # Cor de fundo
+        self.tela.fill((40, 44, 52)) # cor de fundo
         
-        self.player.desenhar(self.tela)
+        self.player.desenhar(self.tela) # desenha o jogador
+        
+        # desenha os projeteis do grupo 
+        self.grupo_projeteis.draw(self.tela) 
 
         pygame.display.flip()
+        
+    def atualizar(self):
+        self.player.mover()
+        self.grupo_projeteis.update()
 
-
-def checar_eventos(self):
-    # o pygame tem um sistema de eventos onde ele registra tudo o que acontece, quando o jogador apertar x para fechar a janela, o evento QUIT é registrado e fecha o jogo
-    for evento in pygame.event.get():
-        if evento.type == pygame.QUIT:
-            self.rodando = False
-
-        if evento.type == pygame.KEYDOWN:
-            if evento.key == pygame.K_ESCAPE:
-                self.rodando = False
